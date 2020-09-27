5ae201100d5253c984f7291a439fa6ddfc5a58e4
prajs1 <71959725+prajs1@users.noreply.github.com>
2020-09-27 08:49:13 +0200

    Create podstawy.py

---
+++ podstawy.py
@@ -0,0 +1,20 @@
+print("test")
+
+x = 10.1234
+print(x)
+print("{:.2f}".format(x))
+
+#potegowanie
+pow = 2**10
+print(pow)
+
+'''
+komentarz w wielu liniach
+'''
+
+#konkatenacja + 
+name = input("Podaj swoje imie: ")
+print("Twoje imie: " + name)
+
+surname = input("Podaj swoje nazwisko: ")
+print("Twoje imie: " + name + ", nazwisko: " + surname)
